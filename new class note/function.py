a372485ab9da6c9ae2d83dee0047f2e6af5806dc
Bijay Tamang <bijaytamang4343@gmail.com>
2024-01-10 20:00:26 +0545

    code

--- new class note/function.py
+++ new class note/function.py
@@ -16,6 +16,23 @@
 
 # short hat statement
 
-a =True
-b =2
-print(a+b)
+# a =True
+# b =2
+# print(a+b)
+
+# syntax:
+# def function_num(arg-->formal argument):
+#     #block of code
+# #function_name(are-->actual argument)
+
+# def show():
+#     print('hello wolrd')
+# show()
+# interview question
+a = [1,2,3]
+b = [1,2,3]
+
+print(a is b)
+print(a==b)
+
+
